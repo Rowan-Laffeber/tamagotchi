f4037cf0b72506ac18284850f46938d25aaa6d4b
Nojan0727 <9025261@student.zadkine.nl>
2025-01-15 11:53:57 +0100

    Revert "Merge remote-tracking branch 'origin/tamagotchi' into tamagotchi"
    This reverts commit 7a94af023bb3578ea1ece0bd3195327d7f488e28, reversing
    changes made to 040ecfbd22c16650c73b4a0ef4883a2df4807edc.

--- script/test.js
+++ script/test.js
@@ -1,68 +1,109 @@
-const SPRITE_WIDTH = 320;
-const SPRITE_HEIGHT = 320;
-const BORDER_WIDTH = 0;
-const SPACING_WIDTH = 0;
+const ARTHUR_WIDTH = 320;
+const ARTHUR_HEIGHT = 320;
 
-function spritePositionToImagePosition(row, col) {
+function arthurPositionToImage(row, col) {
     return {
-        x: (
-            BORDER_WIDTH +
-            col * (SPACING_WIDTH + SPRITE_WIDTH)
-        ),
-        y: (
-            BORDER_WIDTH +
-            row * (SPACING_WIDTH + SPRITE_HEIGHT)
-        )
-    }
-}
-var canvas = document
-            .querySelector('canvas');
-var context = canvas
-              .getContext('2d');
-
-var spriteSheetURL = 'assets/img/Arthur-Morgan/Arthur-Morgan-sprite1.png';
-var image = new Image();
-image.src = spriteSheetURL;
-image.crossOrigin = true;
-
-var row = 0;
-var col = 0;
-function animate() {
-    // once we hit the end of a row,
-    // move to the next
-    if (col === 3) {
-        col = 0;
-        row += 1;
-    }
-    // once we finish the last row,
-    // start again
-    if (row === 2) {
-        row = 0;
-    }
-    
-    // make an image position using the 
-    // current row and colum
-    var position = spritePositionToImagePosition(row, col);
-    context.clearRect(
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    );
-    context.drawImage(
-        image,
-        position.x,
-        position.y,
-        SPRITE_WIDTH,
-        SPRITE_HEIGHT,
-        0,
-        0,
-        SPRITE_WIDTH,
-        SPRITE_HEIGHT
-    );
-    col += 1;
+        x: col * ARTHUR_WIDTH,
+        y: row * ARTHUR_HEIGHT,
+    };
 }
 
-image.onload = function() {
-    setInterval(animate, 500);
+const arthurCanvas = document.getElementById("arthurCanvas");
+const arthurContext = arthurCanvas.getContext("2d");
+
+// Fullscreen width and position Arthur at the bottom
+arthurCanvas.width = ARTHUR_WIDTH;
+arthurCanvas.height = ARTHUR_HEIGHT;
+arthurCanvas.style.position = "absolute";
+arthurCanvas.style.bottom = "0";
+arthurCanvas.style.left = "50%";
+arthurCanvas.style.transform = "translateX(-50%)";
+
+const arthurImage = new Image();
+arthurImage.src = "assets/img/Arthur-Morgan/Arthur-Morgan-sprite1.png";
+arthurImage.crossOrigin = true;
+
+let arthurRow = 0;
+let arthurCol = 0;
+
+function animateArthur() {
+    if (arthurCol === 3) {
+        arthurCol = 0;
+        arthurRow += 1;
+    }
+    if (arthurRow === 2) {
+        arthurRow = 0;
+    }
+
+    const position = arthurPositionToImage(arthurRow, arthurCol);
+    arthurContext.clearRect(0, 0, arthurCanvas.width, arthurCanvas.height);
+    arthurContext.drawImage(
+        arthurImage,
+        position.x, position.y, ARTHUR_WIDTH, ARTHUR_HEIGHT,
+        0, 0, ARTHUR_WIDTH, ARTHUR_HEIGHT
+    );
+    arthurCol += 1;
+}
+
+arthurImage.onload = function () {
+    setInterval(animateArthur, 500);
 };
+
+// Levels Animation Configuration
+const TILE_WIDTH = 130;
+const TILE_HEIGHT = 130;
+const LEVELS_COLUMNS = 1;
+const LEVELS_ROWS = 8;
+const TOTAL_FRAMES = 8;
+
+function levelsPositionToImage(row, col) {
+    return {
+        x: col * TILE_WIDTH,
+        y: row * TILE_HEIGHT,
+    };
+}
+
+const levelsCanvas = document.getElementById("levelsCanvas");
+const levelsContext = levelsCanvas.getContext("2d");
+levelsCanvas.width = TILE_WIDTH;
+levelsCanvas.height = TILE_HEIGHT;
+levelsCanvas.style.position = "absolute";
+levelsCanvas.style.top = "20px";
+levelsCanvas.style.left = "20px";
+
+const levelsImage = new Image();
+levelsImage.src = "assets/img/LEVELS/levels.png";
+levelsImage.crossOrigin = true;
+
+let currentFrame = 0;
+let currentTop = 20;
+
+function animateLevels() {
+    const levelsRow = Math.floor(currentFrame / LEVELS_COLUMNS);
+    const levelsCol = currentFrame % LEVELS_COLUMNS;
+    const position = levelsPositionToImage(levelsRow, levelsCol);
+    levelsContext.clearRect(0, 0, levelsCanvas.width, levelsCanvas.height);
+    levelsContext.drawImage(
+        levelsImage,
+        position.x, position.y, TILE_WIDTH, TILE_HEIGHT, // Source rectangle
+        0, 0, TILE_WIDTH, TILE_HEIGHT // Destination rectangle
+    );
+}
+
+levelsCanvas.addEventListener("click", function () {
+    currentFrame += 1;
+    if (currentFrame === TOTAL_FRAMES) {
+        currentFrame = 0;
+    }
+    currentTop -= 50;
+    if (currentTop < 0) {
+        currentTop = 0;
+    }
+    levelsCanvas.style.top = `${currentTop}px`;
+    animateLevels();
+});
+levelsImage.onload = function () {
+    animateLevels(); // Draw the first frame
+};
+
+// End Level
