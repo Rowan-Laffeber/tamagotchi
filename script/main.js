e55257f6bab15735f0b39721b2850e91c1ee475e
Rowan-Laffeber <9026051@student.zadkine.nl>
2025-01-15 12:25:32 +0100

    arthur working on index

--- script/main.js
+++ script/main.js
@@ -92,7 +92,7 @@ function smoke() {
     }
     document.getElementById("countdown3").innerText = 'deadeye ' + deadeye;
     clearInterval(teller_fuction_deadeye);
-    deadeye_timer()
+    deadeye_timer() 
 }
 function hunger_timer(){
     var teller_fuction_hunger = setInterval(function () {
@@ -276,46 +276,7 @@ window.onload = function() {
 };
 // end music
 // Sprite Arthur
-const canvas = document.getElementById('spriteCanvas');
-const ctx = canvas.getContext('2d');
 
-const SPRITE_WIDTH = 320;
-const SPRITE_HEIGHT = 320;
-const IMAGE_URL = 'assets/img/Arthur-Morgan/Arthur-Morgan-right2.png';
-let currentFrame = 0;
-
-const spriteImage = new Image();
-spriteImage.src = IMAGE_URL;
-
-const animationLoop = [0, 1, 2, 3, 4, 5];
-const frameY = 0;
-
-function drawFrame() {
-    const frameX = animationLoop[currentFrame];
-    const x = frameX * SPRITE_WIDTH;
-    const y = frameY * SPRITE_HEIGHT;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.drawImage(spriteImage, x, y, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT);
-}
-
-function animate() {
-    drawFrame();
-    currentFrame++;
-
-    if (currentFrame >= animationLoop.length) {
-        currentFrame = 0;
-    }
-
-    setTimeout(() => {
-        requestAnimationFrame(animate);
-    }, 250);
-}
-
-spriteImage.onload = function() {
-    animate();
-}
 // end Sprite Arthur
 // function insert_arthur(){
 //     var src = document.getElementById("Arthur");
@@ -323,4 +284,72 @@ spriteImage.onload = function() {
 //     img.src = "ArthurMorgan/ArthurMorgan-right.png";
 // }
 // insert_arthur();
-// end clouds
+const SPRITE_WIDTH = 320;
+const SPRITE_HEIGHT = 320;
+const BORDER_WIDTH = 0;
+const SPACING_WIDTH = 0;
+
+function spritePositionToImagePosition(row, col) {
+    return {
+        x: (
+            BORDER_WIDTH +
+            col * (SPACING_WIDTH + SPRITE_WIDTH)
+        ),
+        y: (
+            BORDER_WIDTH +
+            row * (SPACING_WIDTH + SPRITE_HEIGHT)
+        )
+    }
+}
+
+var canvas = document
+            .getElementById('spriteCanvas');
+var context = canvas
+              .getContext('2d');
+
+var spriteSheetURL = 'assets/img/Arthur-Morgan/Arthur-Morgan-sprite1.png';
+var image = new Image();
+image.src = spriteSheetURL;
+image.crossOrigin = true;
+
+var row = 0;
+var col = 0;
+function animate() {
+    // once we hit the end of a row,
+    // move to the next
+    if (col === 3) {
+        col = 0;
+        row += 1;
+    }
+    // once we finish the last row,
+    // start again
+    if (row === 2) {
+        row = 0;
+    }
+    
+    // make an image position using the 
+    // current row and colum
+    var position = spritePositionToImagePosition(row, col);
+    context.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+    context.drawImage(
+        image,
+        position.x,
+        position.y,
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT,
+        0,
+        0,
+        SPRITE_WIDTH,
+        SPRITE_HEIGHT
+    );
+    col += 1;
+}
+
+image.onload = function() {
+    setInterval(animate, 500);
+};
