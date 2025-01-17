08f9f5e1f9eaf47e48c43bcb205c958b3fcfaaeb
Rowan Laffeber <9026051@student.zadkine.nl>
2025-01-17 15:47:43 +0100

    added pixelated start menu

--- script/main.js
+++ script/main.js
@@ -1074,7 +1074,7 @@ var ArthurContext = ArthurCanvas.getContext('2d');
 
 var ArthurImage = new Image();
 ArthurImage.src = "assets/img/Arthur-Morgan/Arthur-Morgan-sprite1.png";
-ArthurImage.crossOrigin = true;
+ArthurImage_arthur.crossOrigin = true;
 ArthurCanvas.width = Arthur_WIDTH;
 ArthurCanvas.height = Arthur_HEIGHT;
 
